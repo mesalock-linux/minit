6199fdc5b1b4cc6fba520f960b56aa149f75d371
Mingshen Sun <sunmingshen@baidu.com>
2017-10-28 20:23:07 -0700

    Run mknode before start a shell

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,33 @@ extern crate libc;
 use libc::waitpid;
 use std::process::Command;
 
-fn main() {
-    let cmd = "/bin/ion";
-    let mut command = Command::new(cmd);
+fn run(line: &str) {
+    let mut args = line.split(' ').map(|arg| {arg.to_string()});
 
-    match command.spawn() {
-        Ok(mut child) => match child.wait() {
-            Ok(_status) => (),
-                Err(err) => println!("[-] init: failed to wait: {}", err)
-        },
-        Err(err) => println!("[-] init: failed to execute: {}", err)
+    if let Some(cmd) = args.next() {
+        match cmd.as_str() {
+            _ => {
+                let mut command = Command::new(cmd);
+                for arg in args {
+                    command.arg(arg);
+                }
+
+                match command.spawn() {
+                    Ok(mut child) => match child.wait() {
+                        Ok(_status) => (),
+                        Err(err) => println!("[-] init: failed to wait: {}", err)
+                    },
+                    Err(err) => println!("[-] init: failed to execute: {}", err)
+                }
+            }
+        }
+
     }
+}
+
+fn main() {
+    run("/bin/mknod /dev/urandom c 1 9");
+    run("/bin/ion");
 
     loop {
         let mut status = 0;
