91e4842ba74f17ed7ffde231513d904b6d904d13
Mingshen Sun <sunmingshen@baidu.com>
2017-11-09 14:39:56 -0800

    Reorganize source code

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 extern crate libc;
+extern crate nix;
+use nix::unistd;
 
 use libc::{waitpid, sigprocmask, sigfillset, sigset_t, signal};
 use std::os::unix::process::CommandExt;
 use std::process::Command;
 use std::mem;
 use std::ptr;
+use std::ffi::CString;
 
 fn run(line: &str) {
+    println!("[+] init: run {}", line);
     let mut args = line.split(' ').map(|arg| {arg.to_string()});
 
     if let Some(cmd) = args.next() {
@@ -19,13 +23,15 @@ fn run(line: &str) {
 
                 match command.before_exec(|| {
                     unsafe { reset_sighandlers_and_unblock_sigs() }
+                    // TODO: Open the new terminal device
                     Ok(())
                 }).spawn() {
-                    Ok(mut child) => {
-                        match child.wait() {
-                            Ok(_status) => (),
-                            Err(err) => println!("[-] init: failed to wait: {}", err)
-                        }
+                    Ok(mut child) => match child.wait() {
+                        Ok(_status) => {
+                            println!("[+] init: {} exit", line);
+                            unsafe { sigprocmask_allsigs(libc::SIG_UNBLOCK); }
+                        },
+                        Err(err) => println!("[-] init: failed to wait: {}", err)
                     },
                     Err(err) => println!("[-] init: failed to execute: {}", err)
                 }
@@ -35,32 +41,14 @@ fn run(line: &str) {
     }
 }
 
-unsafe fn unblock_signals() {
+unsafe fn sigprocmask_allsigs(how: libc::c_int) {
     let mut sigset = mem::uninitialized::<sigset_t>();
     sigfillset(&mut sigset as *mut sigset_t);
-    sigprocmask(libc::SIG_UNBLOCK, &sigset as *const sigset_t, ptr::null_mut() as *mut sigset_t);
+    sigprocmask(how, &sigset as *const sigset_t, ptr::null_mut() as *mut sigset_t);
 }
 
-unsafe fn block_signals() {
-    let mut sigset = mem::uninitialized::<sigset_t>();
-    sigfillset(&mut sigset as *mut sigset_t);
-    sigprocmask(libc::SIG_BLOCK, &sigset as *const sigset_t, ptr::null_mut() as *mut sigset_t);
-}
 
 unsafe fn reset_sighandlers_and_unblock_sigs() {
-    /*
-     * bb_signals(0
-		+ (1 << SIGUSR1)
-		+ (1 << SIGUSR2)
-		+ (1 << SIGTERM)
-		+ (1 << SIGQUIT)
-		+ (1 << SIGINT)
-		+ (1 << SIGHUP)
-		+ (1 << SIGTSTP)
-		+ (1 << SIGSTOP)
-		, SIG_DFL);
-        */
-    println!("reset_sighandlers_and_unblock_sigs");
     signal(libc::SIGUSR1, libc::SIG_DFL);
     signal(libc::SIGUSR2, libc::SIG_DFL);
     signal(libc::SIGTERM, libc::SIG_DFL);
@@ -69,14 +57,31 @@ unsafe fn reset_sighandlers_and_unblock_sigs() {
     signal(libc::SIGHUP, libc::SIG_DFL);
     signal(libc::SIGTSTP, libc::SIG_DFL);
     signal(libc::SIGSTOP, libc::SIG_DFL);
-    unblock_signals();
+    sigprocmask_allsigs(libc::SIG_UNBLOCK);
 }
 
 fn main() {
-    unsafe { block_signals() }
-    unsafe { reset_sighandlers_and_unblock_sigs() }
-    run("/bin/busybox sh");
+    println!("init");
+    unistd::setsid().expect("setsid failed");
+    unsafe {
+        libc::putenv(CString::new("HOME=/").unwrap().into_raw());
+        libc::putenv(CString::new("PATH=/sbin:/bin:/usr/sbin:/usr/bin").unwrap().into_raw());
+        libc::putenv(CString::new("SHELL=/bin/sh").unwrap().into_raw());
+    }
 
+    // TODO: setup signal handler
+
+    run("/bin/busybox mount -n -t proc proc /proc");
+    run("/bin/busybox mount -n -t devtmpfs devtmpfs /dev");
+    run("/bin/busybox mount -n -t sysfs sysfs /sys");
+
+    run("mknod -m 600 /dev/console c 5 1");
+    run("mknod -m 620 /dev/tty1 c 4 1");
+    run("mknod -m 666 /dev/tty c 5 0");
+    run("mknod -m 666 /dev/null c 1 3");
+    run("mknod -m 660 /dev/kmsg c 1 11");
+
+    run("/bin/mgetty");
     loop {
         let mut status = 0;
         unsafe {
