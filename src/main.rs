10f1cc207e7821e0184439da7da2a40bc1a9a1b3
Mingshen Sun <sunmingshen@baidu.com>
2017-11-22 17:38:55 -0800

    Add license headers in the source code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+// Copyright (c) 2017, MesaLock Linux Authors.
+// All rights reserved.
+// 
+// This work is licensed under the terms of the BSD 3-Clause License.
+// For a copy, see the LICENSE file.
+
 extern crate libc;
 extern crate nix;
 use nix::unistd;
